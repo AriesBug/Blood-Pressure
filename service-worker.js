7566083a8310cd625634a3fcfca5137493b5d35e
AriesBug <arta.averia.ui@phinmaed.com>
2026-03-19 09:53:26 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,11 @@
-const CACHE_NAME = 'v1-assets';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  'bp192.png', // Cache your specific icon files
-  'BP512.png'
-];
+self.addEventListener("install", function(e) {
+    e.waitUntil(
+        caches.open("todo-cache").then(function(cache) {
+            return cache.addAll([
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+                "/index.html"
+                
+            ]
+        )}
+    ));
 });
