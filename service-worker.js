bcebec819d7b4824460edcdcb5e02d17fbd32b53
AriesBug <arta.averia.ui@phinmaed.com>
2026-03-21 09:33:23 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,25 @@
-caches.open("todo-cache-v2").then(cache => {
-    return cache.addAll([
-        "/",
-        "/index.html",
-        "/bp192.png",
-        "/bp512.png"
-    ]);
+self.addEventListener("install", event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open("todo-cache-v2").then(cache => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/bp192.png",
+                "/bp512.png"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
